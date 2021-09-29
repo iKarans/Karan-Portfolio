504a5b434be27f4d308c8f6ba80a3eccaff6e834
iKarans <68602829+iKarans@users.noreply.github.com>
2021-09-29 20:17:57 +0100

    added typewriter effect

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log(7);
+const typewriterText = document.querySelector("#typewriterText");
+const greetings = ["Hey.", "こにちは。", "வணக்கம்.", "Hola.", "Hallo.", "Ciao.", "你好。"]; 
+let i = 0;
+let j = 0;
+let wordArray = [];
+
+function typewrite () {
+    typewriterText.innerText = wordArray.join("");
+    if(i < greetings.length) {
+        if(j < greetings[i].length) {
+            wordArray.push(greetings[i][j])
+            j++
+        } else {
+            wordArray = [];
+            i++
+            j = 0;
+        }
+    } else{
+        i = 0;
+    }
+    setTimeout(typewrite, 500);
+}
+typewrite();
