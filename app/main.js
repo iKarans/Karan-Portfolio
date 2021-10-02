ed489e3cd5a26fa8afefb0e9e19918f033e6a189
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-02 23:59:23 +0100

    got the colors changing for filter list

--- app/main.js
+++ app/main.js
@@ -5,21 +5,47 @@ const javascriptFilter = document.querySelector(".portfolio__filter__javascript"
 const reactFilter = document.querySelector(".portfolio__filter__react");
 const javaFilter = document.querySelector(".portfolio__filter__java");
 const flipCards = document.querySelectorAll(".portfolio__gallery__flip");
+const filterBtns = document.querySelectorAll(".portfolio__filter__btn");
 
-allFilter.addEventListener("click", () => {
+let currentFilter;
+
+allFilter.addEventListener("click", (e) => {
+    currentFilter = allFilter.innerText;
+    console.log(currentFilter);
     flipCards.forEach((card) => {
         card.classList.remove("portfolio__gallery__flip__hide-flip");
-    })
+    });
+    filterBtns.forEach((btn) => {
+        if(btn.innerText == currentFilter) {
+            btn.style.backgroundColor = "#2667FF";
+            btn.style.padding = "5px;"
+        } else {
+            btn.style.backgroundColor = "transparent";
+            btn.style.padding = "0px;"
+        }
+    });
 });
 
 javascriptFilter.addEventListener("click", () => {
     flipCards.forEach((card) => {
+        currentFilter = javascriptFilter.innerText;
+        console.log(currentFilter);
         card.classList.remove("portfolio__gallery__flip__hide-flip");
         if(card.dataset.lang != "javascript") {
             card.classList.add("portfolio__gallery__flip__hide-flip");
         }
-    })
-})
+    });
+    filterBtns.forEach((btn) => {
+        if(btn.innerText == currentFilter) {
+            btn.style.backgroundColor = "#2667FF";
+            btn.style.padding = "5px;"
+        } else {
+            btn.style.backgroundColor = "transparent";
+            btn.style.padding = "0px;"
+        }
+    });
+});
+
 
 reactFilter.addEventListener("click", () => {
     console.log(3);
