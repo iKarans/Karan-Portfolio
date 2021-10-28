c9c7684b3b431dffdf7885337444865683e4b8d9
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-28 21:27:22 +0100

    added particle background

--- app/main.js
+++ app/main.js
@@ -94,3 +94,9 @@ cppFilter.addEventListener("click", () => {
     });
 });
 typewrite();
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+  
